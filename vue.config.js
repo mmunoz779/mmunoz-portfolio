a7d9f67359aec84638e811e03df8c833fc62b30b
Michael Munoz <mmunoz779@outlook.com>
2020-08-21 18:55:18 -0400

    Added minifiers, updated public path for production

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,50 @@
+const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
-    publicPath: "/",
     outputDir: "docs",
-    configureWebpack: {
-        resolve: {
-            alias: {
-                'vue$': 'vue/dist/vue.esm.js'
-            }
-        },
-        devServer: {
-            allowedHosts: [
-                'www.mmunoz.space',
-                'localhost',
-                '127.0.0.1',
-                '::1',
-                '192.168.1.50'
-            ]
+    publicPath: process.env.NODE_ENV === "production" ? "/mmunoz-portfolio" : "/",
+    configureWebpack: config => {
+        // config.resolve = {alias: {'vue$': 'vue/dist/vue.esm.js'}};
+        if (process.env.NODE_ENV === 'production') {
+            config.devtool = '#source-map';
+            // http://vue-loader.vuejs.org/en/workflow/production.html
+            config.plugins = (config.plugins || []).concat([
+                new webpack.DefinePlugin({
+                    'process.env': {
+                        NODE_ENV: '"production"'
+                    }
+                }),
+                new webpack.LoaderOptionsPlugin({
+                    minimize: true,
+                    minimizer: [new TerserPlugin({
+                        cache: true,
+                        parallel: true,
+                        minify: (file, sourceMap) => {
+                            const extractedComments = [];
+
+                            // Custom logic for extract comments
+
+                            const { map, code } = require('terser')
+                                .minify(file, {
+                                    compress: { toplevel: true}
+                                });
+
+                            return { map, code, extractedComments };
+                        }
+                    })]
+                })
+            ]);
+        } else {
+            config.devServer = {
+                allowedHosts: [
+                    'www.mmunoz.space',
+                    'localhost',
+                    '127.0.0.1',
+                    '::1',
+                    '192.168.1.50'
+                ]
+            };
         }
     }
 }
